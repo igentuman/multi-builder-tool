c15a5cd3dfde63803c848dce097ab6cff9a5d374
igentuman <igentuman@gmail.com>
2024-10-02 22:50:47 +0200

    Version 1.0.4

--- src/main/java/igentuman/mbtool/Mbtool.java
+++ src/main/java/igentuman/mbtool/Mbtool.java
@@ -27,7 +27,7 @@ public class Mbtool
 {
     public static final String MODID = "mbtool";
     public static final String NAME = "Multi Builder Tool";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.4";
 
     @Mod.Instance(MODID)
     public static Mbtool instance;
